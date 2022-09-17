acb327eb8a7d730bd7300cc03e9553fad517add6
whoisarjen <Kamilow97@gmail.com>
2022-09-17 18:48:20 +0200

    clean project

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,13 +1,7 @@
-import Table from "@/components/Table/Table"
-import Sidebar from "@/containers/dashboard/Sidebar"
-
 const Dashboard = () => {
     return (
-        <main className="flex flex-auto">
-            <Sidebar />
-            <article className="w-full flex items-center justify-center">
-                <Table />
-            </article>
+        <main>
+            Dashboard
         </main>
     )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,12 +3,7 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        primary: '#d3e3fd',
-        secondary: '#eaf1fb',
-      }
-    },
+    extend: {},
   },
   plugins: [],
 };
