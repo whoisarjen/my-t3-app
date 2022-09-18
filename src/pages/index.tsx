0b0f78f206e54f87e01e0de94572e0f5861096db
whoisarjen <Kamilow97@gmail.com>
2022-09-18 22:39:20 +0200

    v

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,10 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
     return (
-        <>Home</>
+        <>
+            Home:
+            mamy MUI + Tailwind
+        </>
     );
 };
 
